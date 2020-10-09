6064359e5bca8206009f97bae440eb577a8afa34
Lucas Ariel <ylucasx3@gmail.com>
2020-10-09 00:25:36 -0300

    feat(order) listando orders

---
+++ src/app/models/order/order-list.model.ts
@@ -0,0 +1,8 @@
+export class OrderListModel {
+    id: number = 0;
+    customerId: number = 0;
+    status: string = '';
+    amount : number = 0;
+    freight : number = 0;
+    total : number = 0;
+}
--- src/app/utils/service-urls.utils.ts
+++ src/app/utils/service-urls.utils.ts
@@ -1,5 +1,6 @@
 export enum ServiceURLs {
 
     PRODUCTS = '/products',
-
+    
+    ORDERS = '/orders',
 }
